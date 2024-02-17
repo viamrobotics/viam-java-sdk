f480a1a447d228130ca7db5d19e400f5f0646c41
Eric Daniels <eric@erdaniels.com>
2024-02-17 00:42:07 -0500

    fix for security context

--- android/module-plugin/src/main/resources/mod.sh
+++ android/module-plugin/src/main/resources/mod.sh
@@ -6,6 +6,7 @@ export ANDROID_DATA="/data"
 export ANDROID_ROOT="/system"
 export ANDROID_TZDATA_ROOT="/apex/com.android.tzdata"
 export ANDROID_RUNTIME_ROOT="/apex/com.android.runtime"
+export BOOTCLASSPATH=/apex/com.android.runtime/javalib/core-oj.jar:/apex/com.android.runtime/javalib/core-libart.jar:/apex/com.android.runtime/javalib/okhttp.jar:/apex/com.android.runtime/javalib/bouncycastle.jar:/apex/com.android.runtime/javalib/apache-xml.jar:/system/framework/framework.jar:/system/framework/ext.jar:/system/framework/telephony-common.jar:/system/framework/voip-common.jar:/system/framework/ims-common.jar:/system/framework/android.test.base.jar:/apex/com.android.conscrypt/javalib/conscrypt.jar:/apex/com.android.media/javalib/updatable-media.jar
 
 CWD=`pwd`
 JAR_PATH=__MODULE_JAR_PATH__
